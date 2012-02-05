7c6fc9ac4fc05053f9e8a999e983929c2f08067b
Gary Foster <gary.foster@gmail.com>
2012-02-04 19:17:19 -0800

    Initial Commit

---
+++ fractionator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fractionator
+//
+//  Created by Gary Foster on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
